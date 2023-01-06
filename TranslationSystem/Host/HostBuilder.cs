596053726084efbaf5fd79d71b83da8a9997570c
Nikita <nik.lizard.mobile@gmail.com>
2023-01-06 19:47:39 +0400

    Add Dependency Injection

---
+++ TranslationSystem/Host/HostBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace TranslationSystem.Host;
+
+internal static class HostBuilder
+{
+    public static IHost BuildHost()
+    {
+        var host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+            .ConfigureServices(ServiceConfiguration.ConfigureServices);
+        return host.Build();
+    }
+}
+
