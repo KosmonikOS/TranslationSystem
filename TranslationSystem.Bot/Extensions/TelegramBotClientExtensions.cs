c0db42c31b7208c38a261747ad5492f6ee419e85
Nikita <nik.lizard.mobile@gmail.com>
2023-01-21 14:06:07 +0400

    Add extension to handle validational errors and send the back

---
+++ TranslationSystem.Bot/Extensions/TelegramBotClientExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TranslationSystem.Bot.Extensions;
+
+public static class TelegramBotClientExtensions
+{
+    public static async Task SendValidationalErrorAsync(this ITelegramBotClient client, ChatId chatId, IEnumerable<string> errors)
+    {
+        await client.SendTextMessageAsync(chatId, BuildErrorMessage(errors));
+    }
+
+    private static string BuildErrorMessage(IEnumerable<string> errors)
+    {
+        var sb = new StringBuilder("Некорректные аргументы:\n");
+
+        foreach (var error in errors)
+        {
+            sb.Append(error);
+            sb.Append('\n');
+        }
+
+        return sb.ToString();
+    }
+}
+
