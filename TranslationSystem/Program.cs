3ae7547a5dcebd06725e47c1790190da9c1ceff4
Nikita <nik.lizard.mobile@gmail.com>
2023-01-28 12:17:25 +0400

    Add startup of the bot

--- TranslationSystem/Program.cs
+++ TranslationSystem/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using TranslationSystem.Bot.Abstractions;
+using TranslationSystem.Host;
+
+var host = HostBuilder.BuildHost();
+
+var clinet = host.Services.GetRequiredService<ITelegramBotClientWithCommands>();
+
+var cancellationToken = new CancellationTokenSource();
+
+await clinet.StartHandlingCommandsAsync(host.Services,cancellationToken.Token);
+
+Console.ReadLine();
+
+cancellationToken.Cancel();
