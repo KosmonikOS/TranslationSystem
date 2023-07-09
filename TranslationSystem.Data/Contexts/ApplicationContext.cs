b227720f8e4cfb53008eeb475b4a01af35cfd547
Nikita <nik.lizard.mobile@gmail.com>
2023-07-09 18:24:11 +0400

    Add ability to create MongoDb sessions from context

--- TranslationSystem.Data/Contexts/ApplicationContext.cs
+++ TranslationSystem.Data/Contexts/ApplicationContext.cs
@@ -6,11 +6,15 @@ namespace TranslationSystem.Data.Contexts;
 
 public class ApplicationContext : MongoDbContext
 {
+    private readonly MongoClient _client;
     public ApplicationContext(MongoClient client, string dataBaseName)
         : base(client, dataBaseName)
-    { }
+    {
+        _client = client;
+    }
 
     public IMongoCollection<Word> Words => Database.GetCollection<Word>("words");
     public IMongoCollection<UserQuiz> UserQuiz => Database.GetCollection<UserQuiz>("user_quiz");
+    public Task<IClientSessionHandle> CreateSessionAsync() => _client.StartSessionAsync();
 }
 
